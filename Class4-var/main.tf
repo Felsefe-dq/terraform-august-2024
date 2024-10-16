55921389278a915f61e9280c56d733f0594ac48a
Felsefe-dq <devopsdatcom@gmail.com>
2024-10-16 02:01:50 +0000

    added new files

---
+++ Class4-var/main.tf
@@ -0,0 +1,99 @@
+provider aws {
+    region =  var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet1_cidr
+availability_zone = "${var.region}a"
+map_public_ip_on_launch =  var.ip_on_launch
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet2_cidr
+availability_zone = "${var.region}b"
+map_public_ip_on_launch = var.ip_on_launch
+  tags = {
+    Name = "subnet2"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.main2.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_size
+
+  #availability_zone = "us-east-2b"
+   subnet_id = aws_subnet.main.id
+   key_name = aws_key_pair.deployer.key_name
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+   user_data = file("apache.sh")
+   
+   
+}
+
+output ec2 {
+    value = aws_instance.web.public_ip
+}
